bbdf2e9d3d8bd7a9f2fad7be36ead06f2bd0ad5c
subhajitdas1999 <sd651846@gmail.com>
2023-01-03 15:51:02 +0530

    Initial file

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 import express from "express";
-import morgan from "morgan";
-import rateLimiter from "express-rate-limiter";
-import cookieParser from "cookie-parser";
-import hpp from "hpp";
-import mogulRentalRoutes from "./routes/rentalRoutes.js";
+// import dotenv from "dotenv";
+// import morgan from "morgan";
+// import rateLimiter from "express-rate-limiter";
+// import cookieParser from "cookie-parser";
+// import hpp from "hpp";
+// import mogulRentalRoutes from "./routes/rentalRoutes.js";
+import voterRouter from "./routes/voterRoutes.js";
+import proposeRouter from "./routes/proposeRoutes.js";
+import globalErrorHandler from "./controllers/globalErrorHandler.js";
 
 const app = express();
 
-const limiter = rateLimiter({
-	windowMs: 15 * 60 * 1000,
-	max: 100,
-	standardHeaders: true,
-	legacyHeaders: false,
-});
+// const limiter = rateLimiter({
+//   windowMs: 15 * 60 * 1000,
+//   max: 100,
+//   standardHeaders: true,
+//   legacyHeaders: false,
+// });
 
-app.use(morgan("dev"));
-app.use(limiter);
+// app.use(morgan("dev"));
+// app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(hpp());
+// app.use(cookieParser());
+// app.use(hpp());
 
-app.use("/api/v1/mogulRental", mogulRentalRoutes);
+// app.use("/api/v1/mogulRental", mogulRentalRoutes);
 
-app.all("*", (req, res) => {
-	res.status(400).json({
-		status: "Fail",
-		message: "did not find any matching route",
-	});
+app.use("/api/propose", proposeRouter);
+app.use("/api/vote", voterRouter);
+
+//if no route hit till this point
+app.all("*", (req, res, next) => {
+  res.status(204).json({
+    status: "Not a valid path",
+    result: "Server is running",
+  });
 });
+
+//global error handler
+app.use(globalErrorHandler);
 
 export default app;
