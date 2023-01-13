3abfd49b9974fa2785a104f171622c858ed93f7d
subhajitdas1999 <sd651846@gmail.com>
2023-01-13 13:25:13 +0530

    fixed voter proof functionality

--- controllers/voterController.js
+++ controllers/voterController.js
@@ -1,7 +1,9 @@
+/* eslint-disable prefer-const */
 /* eslint-disable prettier/prettier */
 /* eslint-disable consistent-return */
 /* eslint-disable import/extensions */
 import { Network, Alchemy } from "alchemy-sdk";
+import keccak256 from "keccak256";
 import dotenv from "dotenv";
 import catchAsync from "../utils/catchAsync.js";
 import ProposalModel from "../models/proposalModel.js";
@@ -12,55 +14,56 @@ import { filterObj } from "../utils/helper.js";
 dotenv.config({ path: "../.env" });
 
 const getVotersDetails = catchAsync(async (req, res, next) => {
-	// alchemy settings
-	const settings = {
-		apiKey: process.env.ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
-		network: Network.MATIC_MUMBAI, // Replace with your network.
-	};
-	const alchemy = new Alchemy(settings);
-
 	// filter the req.body data
-	const newBody = filterObj(req.query, "tokenId", "target", "owner");
+	const newBody = filterObj(req.query, "tokenId", "target", "owner", "onChainProposalId");
 
 	if (!newBody.target) {
 		return next("target is required");
 	}
-	if (
-		(newBody.target === "voterAddresses" || newBody.target === "voterRootHash") &&
-		!newBody.tokenId
-	) {
+	if (newBody.target === "voterRootHash" && !newBody.tokenId) {
 		return next("tokenId is required");
 	}
-	if (newBody.target === "voterProof" && !newBody.tokenId && !newBody.owner) {
-		return next("tokenId , owner is required");
+	if (
+		newBody.target === "voterProof" &&
+		!newBody.tokenId &&
+		!newBody.owner &&
+		!newBody.onChainProposalId
+	) {
+		return next("tokenId ,onChainProposalId, owner is required");
 	}
-	const { owners } = await alchemy.nft.getOwnersForNft(
-		process.env.PROPERTY_TOKEN_ADDRESS,
-		newBody.tokenId
-	);
 
-	// const owners = [
-	// 	"0x271a102ab92f9584f3a83db17f3d766e1719ee3e",
-	// 	"0x2c2349ba934b4d50cd9c1335d7009b92bf8e4661",
-	// ];
-	//   console.log(req.body);
-	const merkleTree = new MogulDAOMarkleTree(owners);
 	let result;
-	if (newBody.target === "voterAddresses") {
-		result = {
-			length: owners.length,
-			data: owners,
-		};
-	}
+	// if (newBody.target === "voterAddresses") {
+	// 	result = {
+	// 		length: owners.length,
+	// 		data: owners,
+	// 	};
+	// }
 	if (newBody.target === "voterRootHash") {
+		// alchemy settings
+		const settings = {
+			apiKey: process.env.ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
+			network: Network.MATIC_MUMBAI, // Replace with your network.
+		};
+		const alchemy = new Alchemy(settings);
+		const { owners } = await alchemy.nft.getOwnersForNft(
+			process.env.PROPERTY_TOKEN_ADDRESS,
+			newBody.tokenId
+		);
+
+		let merkleTree = new MogulDAOMarkleTree(owners);
 		result = {
 			data: merkleTree.getOwnersRootHash(),
 		};
 	}
 	if (newBody.target === "voterProof") {
-		if (!newBody.owner) {
-			return next("owner Address is required to fetch merkle proof");
-		}
+		const proposalIdHash = keccak256(
+			newBody.tokenId.toString() + newBody.onChainProposalId.toString()
+		).toString("hex");
+		const proposalDetails = await ProposalModel.findOne({ proposalIdHash });
+		const owners = proposalDetails.ownerListAtProposal;
+
+		let merkleTree = new MogulDAOMarkleTree(owners);
 		result = { data: merkleTree.getOwnerProof(newBody.owner) };
 	}
 	res.status(200).json({
